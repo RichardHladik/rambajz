8855a6addac758fbe8655022005fe7d86722f7e7
Richard Hladík <rihl@uralyx.cz>
2019-08-19 15:03:22 +0200

    buffer: store the "absolute value" of buf->e instead of taking it % size

--- src/buffer.c
+++ src/buffer.c
@@ -4,6 +4,6 @@ void buffer_push(struct buffer *buf, double *data, size_t n)
 {
 	size_t e = buf->e;
 	for (size_t i = 0; i < n; i++)
-		buf->data[(e + i) % buf->size] = data[i];
-	buf->e = (e + n) % buf->size;
+		buf->data[(e + i) & (buf->size - 1)] = data[i];
+	buf->e = e + n;
 }
--- src/buffer.h
+++ src/buffer.h
@@ -5,7 +5,7 @@ struct buffer {
 	double *data;
 	size_t s;
 	_Atomic size_t e;
-	size_t size;
+	size_t size; // Always a power of two
 };
 
 void buffer_push(struct buffer *buf, double *data, size_t n);
