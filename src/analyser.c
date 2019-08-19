3eea7854de35759e84792ce492d371e1e0f6d3c6
Richard Hladík <rihl@uralyx.cz>
2019-08-19 15:58:28 +0200

    analyser: add smoothing

--- src/analyser.c
+++ src/analyser.c
@@ -1,5 +1,6 @@
 #include <math.h>
 #include <malloc.h>
+#include <memory.h>
 #include "analyser.h"
 
 const int FRAME_SIZE = (1 << 12);
@@ -18,12 +19,18 @@ struct analysis_data *analyse(struct analysis_data *data, struct buffer *buf)
 	data->guessed_frequency = -1;
 	data->plot_size = (n + 1) / 2;
 	data->plot = malloc(data->plot_size * sizeof(*data->plot));
+
+	// for now, assume fft_slow returns the "same" plot every time (only with different y values)
 	fft_slow(n, v, data->plot_size, data->plot);
-	// FIXME: smoothing doesn't work yet
 	const double SMOOTHING = .5;
-	if (oldplot)
+	if (!oldplot) {
+		oldplot = malloc(data->plot_size * sizeof(*data->plot));
+	} else {
 		for (int i = 0; i < data->plot_size; i++)
 			data->plot[i].y = data->plot[i].y * (1 - SMOOTHING) + oldplot[i].y * SMOOTHING;
+	}
+
+	memcpy(oldplot, data->plot, data->plot_size * sizeof(*data->plot));
 
 finish:
 	free(v);
