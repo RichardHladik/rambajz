80d2cf18c2767405b5b0d9b574ded9efccbaa983
Richard Hladík <rihl@uralyx.cz>
2019-08-22 22:06:18 +0200

    refactor draw, split it into smaller functions

--- src/rambajz.c
+++ src/rambajz.c
@@ -29,7 +29,8 @@ int record(jack_nframes_t nframes, void *arg)
 	return 0;
 }
 
-void draw(size_t n, struct point *data);
+void draw(struct analysis_data *);
+void draw_plot(size_t n, struct point *data);
 
 bool process(struct buffer *buf)
 {
@@ -108,36 +109,47 @@ bool process(struct buffer *buf)
 
 	for (int i = 0; i < data.plot_size; i++)
 		data.plot[i].x = logscale(data.plot[i].x, params.min_freq, params.max_freq);
+	data.guessed_frequency = logscale(data.guessed_frequency, params.min_freq, params.max_freq);
 
-	SDL_Rect rect = {.h = sdl_state.h, .w = sdl_state.w, .x = 0, .y = 0};
-	SDL_SetRenderDrawColor(sdl_state.ren, 0, 0, 0, 255);
-	SDL_RenderClear(sdl_state.ren);
-
-	SDL_SetRenderDrawColor(sdl_state.ren, 255, 255, 255, 255);
-	draw(data.plot_size, data.plot);
-	SDL_RenderPresent(sdl_state.ren);
+	draw(&data);
 
 	analysis_free(data);
 	return true;
 }
 
-void draw(size_t n, struct point *data)
-{
-	for (size_t i = 0; i < n; i++)
-		data[i].x = fmin(1, fmax(0, data[i].x));
+void plot_interval(double x0, double x1, double y) {
+	x0 = fmin(1, fmax(0, x0));
+	x1 = fmin(1, fmax(0, x1));
+	if ((x0 <= 0 && x1 <= 0) || (x0 >= 1 && x1 >= 1))
+		return;
 
 	SDL_Rect rect;
-	for (int i = 0; i < n; i++) {
-		rect.h = sqrt(data[i].y) * sdl_state.h;
-		rect.y = sdl_state.h - rect.h;
-		rect.x = sdl_state.w * ((i) ? data[i - 1].x : 0);
-		rect.w = sdl_state.w * data[i].x - rect.x;
-		if (rect.w <= 0)
-			rect.w = 1;
-		SDL_RenderFillRect(sdl_state.ren, &rect);
-	}
+	rect.h = round(sdl_state.h * y);
+	rect.y = sdl_state.h - rect.h;
+	rect.x = round(sdl_state.w * x0);
+	rect.w = round(sdl_state.w * x1) - rect.x;
+	rect.w = (rect.w <= 0) ? 1 : rect.w;
+	SDL_RenderFillRect(sdl_state.ren, &rect);
+}
 
-	//SDL_Delay(100);
+void draw(struct analysis_data *data) {
+	SDL_Rect rect = {.h = sdl_state.h, .w = sdl_state.w, .x = 0, .y = 0};
+	SDL_SetRenderDrawColor(sdl_state.ren, 0, 0, 0, 255);
+	SDL_RenderClear(sdl_state.ren);
+
+	SDL_SetRenderDrawColor(sdl_state.ren, 255, 255, 255, 255);
+	draw_plot(data->plot_size, data->plot);
+	SDL_RenderPresent(sdl_state.ren);
+}
+
+void draw_plot(size_t n, struct point *data)
+{
+	for (int i = 0; i < n; i++) {
+		double x0 = (i) ? data[i - 1].x : 0;
+		double x1 = data[i].x;
+		double y = sqrt(data[i].y);
+		plot_interval(x0, x1, y);
+	}
 }
 
 int main(void)
