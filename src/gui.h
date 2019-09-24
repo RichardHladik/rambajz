9a1a30e6a9f9f92fd1c38a05dbc87c2cca16df63
Richard Hladík <rihl@uralyx.cz>
2019-09-24 16:17:16 +0200

    Move the rest of the GUI logic to gui.c

--- src/gui.h
+++ src/gui.h
@@ -7,3 +7,6 @@ struct viewport_t {
 };
 
 bool interact(struct analysis_params *params, struct viewport_t *viewport, const struct viewport_t *limits);
+void plot_interval(double x0, double x1, double y);
+void draw_plot(size_t n, const struct point *data, double A, double B);
+void draw(const struct analysis_data *data, const struct analysis_params *params);
--- src/rambajz.c
+++ src/rambajz.c
@@ -26,9 +26,6 @@ int record(jack_nframes_t nframes, void *arg)
 	return 0;
 }
 
-void draw(const struct analysis_data *, const struct analysis_params *);
-void draw_plot(size_t n, const struct point *data, double from, double to);
-
 bool process(struct buffer *buf)
 {
 	const double min_freq = 20;
@@ -52,44 +49,6 @@ bool process(struct buffer *buf)
 
 	analysis_free(data);
 	return true;
-}
-
-void plot_interval(double x0, double x1, double y) {
-	x0 = fmin(1, fmax(0, x0));
-	x1 = fmin(1, fmax(0, x1));
-	if ((x0 <= 0 && x1 <= 0) || (x0 >= 1 && x1 >= 1))
-		return;
-
-	SDL_Rect rect;
-	rect.h = round(sdl_state.h * y);
-	rect.y = sdl_state.h - rect.h;
-	rect.x = round(sdl_state.w * x0);
-	rect.w = round(sdl_state.w * x1) - rect.x;
-	rect.w = (rect.w <= 0) ? 1 : rect.w;
-	SDL_RenderFillRect(sdl_state.ren, &rect);
-}
-
-void draw(const struct analysis_data *data, const struct analysis_params *params) {
-	SDL_Rect rect = {.h = sdl_state.h, .w = sdl_state.w, .x = 0, .y = 0};
-	SDL_SetRenderDrawColor(sdl_state.ren, 0, 0, 0, 255);
-	SDL_RenderClear(sdl_state.ren);
-
-	SDL_SetRenderDrawColor(sdl_state.ren, 255, 255, 255, 255);
-	draw_plot(data->plot_size, data->plot, params->min_freq, params->max_freq);
-	SDL_SetRenderDrawColor(sdl_state.ren, 255, 0, 0, 255);
-	double guessed = logscale(data->guessed_frequency, params->min_freq, params->max_freq);
-	plot_interval(guessed, guessed, 1);
-	SDL_RenderPresent(sdl_state.ren);
-}
-
-void draw_plot(size_t n, const struct point *data, double A, double B)
-{
-	for (int i = 0; i < n; i++) {
-		double x0 = (i) ? data[i - 1].x : 0;
-		double x1 = data[i].x;
-		double y = sqrt(data[i].y);
-		plot_interval(logscale(x0, A, B), logscale(x1, A, B), y);
-	}
 }
 
 int main(void)
